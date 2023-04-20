298823a53cdc5248900c5746d01a50292a18b958
Vasile Bancila <bancilavasile93@gmail.com>
2023-04-20 17:57:48 +0200

    update of the project to the bonus version, which additionally has the functionality of automatically generating buttons

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
-function winner() {
-    alert('Castigator');
-}
-  
-function loser() {
-    alert('Pierzator');
-}
+document.getElementById("generateButtons").onclick = function genBtn() {
+    let numBtn = parseInt(document.getElementById("numButtons").value); //we convert the input from string to int 
+    let boxButtons = document.getElementById("boxBtn");
+    let winningButton = Math.floor(Math.random() * numBtn) + 1; //we choose the random winning button
+    
+    if (numBtn < 1) {
+        alert("Enter a number greater than zero!"); //we check if the input is greater than zero
+        return;
+    }
 
-let random = Math.floor(Math.random() * 3) + 1;
+    boxButtons.innerHTML = ""; //empty the box before creating new buttons
 
-for(var index = 1; index <= 3; ++index) {
-    let button = document.querySelector(`button.btn${index}`);
-    if(random == index) {
-        button.addEventListener('click', winner);
-    } else {
-        button.addEventListener('click', loser);
+    /*We create the buttons, add a "Castigator" event and the green color to the winning button and
+    add a "Pierzator" event and the red color to the losing buttons, then we add them to the button box.*/   
+
+    for (let index = 1; index <= numBtn; ++index) {
+        let newBtn = document.createElement("button");
+        newBtn.type = "button";
+        newBtn.id = `button.btn${index}`;
+        newBtn.textContent = `Button${index}`;
+        if(winningButton == index) {
+            newBtn.addEventListener('click', function winner() {
+                alert('Castigator');
+                newBtn.setAttribute('style', 'background-color: green');
+            });
+        } else {
+            newBtn.addEventListener('click', function loser() {
+                alert('Pierzator');
+                newBtn.setAttribute('style', 'background-color: red');
+            });
+        }
+        boxButtons.appendChild(newBtn);
     }
 }
