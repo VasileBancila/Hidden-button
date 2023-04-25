ddb143e1955c3d675a7ef047431785a0e2d8a0f6
Vasile Bancila <bancilavasile93@gmail.com>
2023-04-25 21:09:20 +0200

    split code into two parts

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-document.getElementById("generateButtons").onclick = function genBtn() {
+function genBtn() {
     let numBtn = parseInt(document.getElementById("numButtons").value); //we convert the input from string to int 
     let boxButtons = document.getElementById("boxBtn");
     let winningButton = Math.floor(Math.random() * numBtn) + 1; //we choose the random winning button
-    
+
     if (numBtn < 1) {
         alert("Enter a number greater than zero!"); //we check if the input is greater than zero
         return;
@@ -10,25 +10,31 @@ document.getElementById("generateButtons").onclick = function genBtn() {
 
     boxButtons.innerHTML = ""; //empty the box before creating new buttons
 
-    /*We create the buttons, add a "Castigator" event and the green color to the winning button and
-    add a "Pierzator" event and the red color to the losing buttons, then we add them to the button box.*/   
+    /*We create the buttons, add a "Castigator" event and
+    add a "Pierzator" event to the losing buttons, then we add them to the button box.*/
 
     for (let index = 1; index <= numBtn; ++index) {
         let newBtn = document.createElement("button");
         newBtn.type = "button";
-        newBtn.id = `button.btn${index}`;
+        newBtn.class = `button.btn${index}`;
         newBtn.textContent = `Button${index}`;
-        if(winningButton == index) {
-            newBtn.addEventListener('click', function winner() {
-                alert('Castigator');
-                newBtn.setAttribute('style', 'background-color: green');
-            });
+        if (winningButton == index) {
+            newBtn.addEventListener('click', buttonSelection.winner);
         } else {
-            newBtn.addEventListener('click', function loser() {
-                alert('Pierzator');
-                newBtn.setAttribute('style', 'background-color: red');
-            });
+            newBtn.addEventListener('click', buttonSelection.loser);
         }
         boxButtons.appendChild(newBtn);
     }
 }
+
+
+
+const buttonSelection = {
+    winner() {
+        alert('Castigator');
+    },
+
+    loser() {
+        alert('Pierzator');
+    },
+};
